b304a8becb2a200d946a400fa1a35dea8a1ebd76
punIN10ded <imnotfreenow@gmail.com>
2023-01-28 19:00:14 +0530

    admin page

--- frontend/src/components/admin/admin.js
+++ frontend/src/components/admin/admin.js
@@ -1,4 +1,16 @@
 import React from "react";
+import coin from "../../assets/coin.png";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const rows = [
+	{ name: "Frozen yoghurt", mrp: 159, sid: 6.0, supplier: 24, action: 4.0 },
+];
 
 function Admin() {
 	return (
@@ -7,10 +19,71 @@ function Admin() {
 				<div className="container">
 					<h1>Welcome back Admin</h1>
 					<div className="card-container">
-						<div className="card">Items</div>
-						<div className="card">Items</div>
-						<div className="card">Items</div>
+						<div className="card">
+							<h1>Total Items</h1>
+							<div className="flex">
+								<img src={coin} alt="" />
+								<p>120</p>
+							</div>
+						</div>
+						<div className="card">
+							<h1>Total Items</h1>
+							<div className="flex">
+								<img src={coin} alt="" />
+								<p>120</p>
+							</div>
+						</div>
+						<div className="card">
+							<h1>Total Items</h1>
+							<div className="flex">
+								<img src={coin} alt="" />
+								<p>120</p>
+							</div>
+						</div>
 					</div>
+					<TableContainer component={Paper}>
+						<Table sx={{ minWidth: 650 }} aria-label="simple table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Product Name</TableCell>
+									<TableCell align="right">MRP</TableCell>
+									<TableCell align="right">
+										Product ID
+									</TableCell>
+									<TableCell align="right">
+										Supplier
+									</TableCell>
+									<TableCell align="right">Action</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{rows.map((row) => (
+									<TableRow
+										key={row.name}
+										sx={{
+											"&:last-child td, &:last-child th":
+												{ border: 0 },
+										}}>
+										<TableCell component="th" scope="row">
+											{row.name}
+										</TableCell>
+										<TableCell align="right">
+											{row.mrp}
+										</TableCell>
+										<TableCell align="right">
+											{row.sid}
+										</TableCell>
+										<TableCell align="right">
+											{row.supplier}
+										</TableCell>
+										<TableCell align="right">
+											{row.action}
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</TableContainer>
 				</div>
 			</div>
 		</>
